4bc75b12536a1b6e6e731ea945cee0e49bbe4dee
jasleen kaur <jjasleenkaur2@rrc.ca>
2026-03-16 16:05:33 -0500

    Initialize backend Express server with TypeScript configuration

---
+++ pixell-river-directory/backend/src-backend/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+import employeeRoutes from "./routes/employeeRoutes";
+import roleRoutes from "./routes/roleRoutes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/employees", employeeRoutes);
+app.use("/roles", roleRoutes);
+
+export default app;
